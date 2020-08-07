db4bb0756ccb39d2d08da11d8d944d75dfbd7522
g-agrawal <er.gourav.agrawal@gmail.com>
2020-08-07 12:52:12 +0530

    changed project logo for mobile screen fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = (props) => {
     return (
      <nav className="nav-wrapper grey darken-3">
          <div className="container">
-             <Link to='/' className="brand-logo">ProjectPlan</Link>
+             <Link to='/' className="brand-logo project-logo left">Project</Link>
              { links }
          </div>
      </nav>
